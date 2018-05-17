7336cb5df13acaed6896c52754cd44ae39cec61b
William Chan <willchan2@gmail.com>
2018-05-16 22:32:09 -0400

    use a flexible port

--- main.go
+++ main.go
@@ -1,20 +1,35 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
+	"net/http/pprof"
 
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 )
 
-func main() {
-	handler := func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprint(w, "Hello World")
-	}
-	r := mux.NewRouter()
-	r.Handle("/", http.HandlerFunc(handler))
+var (
+	port string
+)
 
-	logrus.Fatal(http.ListenAndServe(":8080", r))
+func init() {
+	flag.StringVar(&port, "port", "8080", "port for which to run the web service")
+	flag.Parse()
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	health := func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}
+	r.HandleFunc("/health", health)
+	r.HandleFunc("/health/pprof/heap", pprof.Index)
+	r.HandleFunc("/health/pprof/profile", pprof.Profile)
+
+	svcPort := fmt.Sprintf(":%s", port)
+	logrus.Infof("Running service on port %s", svcPort)
+	logrus.Fatal(http.ListenAndServe(svcPort, r))
 }
